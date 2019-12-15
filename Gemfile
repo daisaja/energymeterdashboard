924d991278fd240a68b6d30b45e4566ef21daca9
daisaja <daisaja@gmx.de>
2019-12-15 10:47:39 +0100

    first version of energymeter dashboard - SMA query and Youless query included

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'smashing'
+
+# energy meter
+gem 'httparty'
---
+++ jobs/ops.rb
@@ -0,0 +1,5 @@
+# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+SCHEDULER.every '1s', :first_in => 0 do |job|
+  send_event('response-time1', { value: (rand * 400).to_i })
+  send_event('response-time2', { value: (rand * 400).to_i })
+end
