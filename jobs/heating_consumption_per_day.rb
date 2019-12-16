553048b55c235505d9a99609806d9274bd2f6d6b
daisaja <daisaja@gmx.de>
2019-12-16 19:49:09 +0100

    Added house watt consumption and per day sum as well as solar per day production sum - solar not implemented yet

---
+++ jobs/heating_consumption_per_day.rb
@@ -0,0 +1,26 @@
+require 'httparty'
+
+# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+SCHEDULER.every '5m', :first_in => 0 do |job|
+  # Verbrauch aktueller Tag
+  url_day_count = 'http://192.168.178.10/V?d=0&f=j'
+  response_day_counts = HTTParty.get(url_day_count)
+  array = response_day_counts.parsed_response['val']
+  day_sum = 0
+  array.each { |a|
+    day_sum += a.to_i
+  }
+  kwh_day = (day_sum/1000)
+
+  # Verbrauch gestriger Tag
+  url_last_day_count = 'http://192.168.178.10/V?d=1&f=j'
+  response_last_day_counts = HTTParty.get(url_last_day_count)
+  array = response_last_day_counts.parsed_response['val']
+  last_day_sum = 0
+  array.each { |a|
+    last_day_sum += a.to_i
+  }
+  kwh_last_day = (last_day_sum/1000)
+
+  send_event('wattmeterheating_sum',   { current: kwh_day, last: kwh_last_day })
+end
