d40b40068ea0991cc9126c1f86832ce9570330ce
daisaja <daisaja@gmx.de>
2019-12-20 19:35:15 +0100

    Added ability to read solar day production in kWh from power inverter as well as from the day before. Added also first draft of Dockerfile.

--- jobs/netwatts.rb
+++ jobs/netwatts.rb
@@ -6,6 +6,6 @@ SCHEDULER.every '1s', :first_in => 0 do |job|
   # Augenblickverbrauch
   url_overview = 'http://192.168.178.10/a?f=j'
   response = HTTParty.get(url_overview)
-  send_event('wattmeterhouse',   { value: response.parsed_response['pwr'] })
+  send_event('wattmeternet',   { value: response.parsed_response['pwr'] })
 
 end
