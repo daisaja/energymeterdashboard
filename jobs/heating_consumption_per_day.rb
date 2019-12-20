66e307b8fe6da7a754ad0ac6f135f595279b8c72
daisaja <daisaja@gmx.de>
2019-12-20 19:43:30 +0100

    Refactored code an removed doubled lines of code to increase maintainibility of the code.

--- jobs/heating_consumption_per_day.rb
+++ jobs/heating_consumption_per_day.rb
@@ -3,24 +3,22 @@ require 'httparty'
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '5m', :first_in => 0 do |job|
   # Verbrauch aktueller Tag
-  url_day_count = 'http://192.168.178.10/V?d=0&f=j'
-  response_day_counts = HTTParty.get(url_day_count)
-  array = response_day_counts.parsed_response['val']
-  day_sum = 0
-  array.each { |a|
-    day_sum += a.to_i
-  }
-  kwh_day = (day_sum/1000)
+  url_current_day_count = 'http://192.168.178.10/V?d=0&f=j'
+  kwh_current_day = calculate_sum_of_watts(url_current_day_count)
 
   # Verbrauch gestriger Tag
   url_last_day_count = 'http://192.168.178.10/V?d=1&f=j'
-  response_last_day_counts = HTTParty.get(url_last_day_count)
-  array = response_last_day_counts.parsed_response['val']
+  kwh_last_day = calculate_sum_of_watts(url_last_day_count)
+
+  send_event('wattmeterheating_sum',   { current: kwh_current_day, last: kwh_last_day })
+end
+
+def calculate_sum_of_watts(data_url_to_fetch_from)
+  response_with_data = HTTParty.get(data_url_to_fetch_from)
+  array = response_with_data.parsed_response['val']
   last_day_sum = 0
   array.each { |a|
     last_day_sum += a.to_i
   }
-  kwh_last_day = (last_day_sum/1000)
-
-  send_event('wattmeterheating_sum',   { current: kwh_day, last: kwh_last_day })
+  return (last_day_sum/1000)
 end
