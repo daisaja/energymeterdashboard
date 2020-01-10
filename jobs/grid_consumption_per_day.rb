2bf637e2db064cb2e2a5544312877b5ca963b76c
Lars Gentsch <lars.gentsch@cloud.ionos.com>
2020-01-10 19:51:25 +0100

    Renamed net to grid.

--- jobs/grid_consumption_per_day.rb
+++ jobs/grid_consumption_per_day.rb
@@ -16,7 +16,7 @@ end
 def calculate_sum_of_watts(data_url_to_fetch_from)
   response_with_data = HTTParty.get(data_url_to_fetch_from)
   array = response_with_data.parsed_response['val']
-  last_day_sum = 0
+  last_day_sum = 0.0
   array.each { |a|
     last_day_sum += a.to_f
   }
