6e515cd552d4ca25c02c4ba30cb2c6354f5913d2
Lars Gentsch <lars.gentsch@cloud.ionos.com>
2020-01-12 11:23:42 +0100

    Fixed float issue in grid consumption to show one digit after comma. Added some log statements for solar summary consumption.

--- jobs/grid_consumption_per_day.rb
+++ jobs/grid_consumption_per_day.rb
@@ -9,16 +9,15 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
   # Verbrauch gestriger Tag
   url_last_day_count = 'http://192.168.178.10/V?d=1&f=j'
   kwh_last_day = calculate_sum_of_watts(url_last_day_count)
-
   send_event('wattmeternet_sum',   { current: kwh_current_day, last: kwh_last_day })
 end
 
 def calculate_sum_of_watts(data_url_to_fetch_from)
   response_with_data = HTTParty.get(data_url_to_fetch_from)
   array = response_with_data.parsed_response['val']
-  last_day_sum = 0.0
+  last_day_sum = 0
   array.each { |a|
-    last_day_sum += a.to_f
+    last_day_sum += a.to_i
   }
-  return (last_day_sum/1000).round(1)
+  return (last_day_sum.to_f/1000).round(1)
 end
