8450dcdc03fddda701cb5e172c8fa0165e559b1e
daisaja <daisaja@gmx.de>
2020-04-30 23:38:52 +0200

    Added first version of heating meter.

--- jobs/energy_meter_summary.rb
+++ jobs/energy_meter_summary.rb
@@ -1,12 +1,16 @@
 require_relative 'meter_helper/grid_meter_client'
+require_relative 'meter_helper/heating_meter_client'
 
 SCHEDULER.every '3s' do
   grid_measurements = fetch_data_from_grid_meter()
   solar_measurements = fetch_data_from_solar_meter()
+  heating_measurements = fetch_data_from_heating_meter()
 
   grid_meter_values = {
     'grid_supply_per_month' => {label: 'Netzbezug (Monat)' , value: grid_measurements.grid_supply_per_month},
     'grid_feed_per_month' => {label: 'Einspeisung (Monat)' , value: grid_measurements.grid_feed_per_month},
+    'heating_per_month' => {label: 'Heizung (Monat)' , value: heating_measurements.heating_per_month},
+    'energy_consumption_per_month' => {label: 'Verbrauch (Monat)' , value: grid_measurements.energy_consumption_per_month}, # not implemented yet
     'grid_supply_total' => {label: 'Bezug (Jahr)' , value: grid_measurements.grid_supply_total},
     'grid_feed_total' => {label: 'Einspeisung (Jahr)' , value: grid_measurements.grid_feed_total},
     'grid_supply_current' => {label: 'Bezug (jetzt)' , value: grid_measurements.grid_supply_current},
--- jobs/meter_helper/solar_meter_client.rb
+++ jobs/meter_helper/solar_meter_client.rb
@@ -1,26 +1,32 @@
 require 'httparty'
 
-SMA_URL = 'https://192.168.178.98/dyn/getDashValues.json'
+SMA_VALUES_URL = 'https://192.168.178.98/dyn/getDashValues.json'
+SMA_LOGGER_URL = 'https://192.168.178.98/dyn/getDashLogger.json'
 
 class SolarMeasurements
-  def initialize(solar_watts_current)
+  def initialize(solar_watts_current, solar_watts_per_month)
      @solar_watts_current = solar_watts_current
+     @solar_watts_per_month = solar_watts_per_month
   end
 
   attr_reader :solar_watts_current
+  attr_reader :solar_watts_per_month
 
   def to_string()
     puts "solar_watts_current: #{@solar_watts_current}"
+    puts "solar_watts_per_month: #{@solar_watts_per_month}"
   end
 end
 
 def fetch_data_from_solar_meter()
-  response = HTTParty.post(SMA_URL, :verify => false) #without ssl check
+  response = HTTParty.post(SMA_VALUES_URL, :verify => false) #without ssl check
   solar_watts_current = response.parsed_response['result']['017A-B339126F']['6100_40263F00']['1'][0]['val']
 
   if solar_watts_current.nil? || solar_watts_current == 0
     solar_watts_current = 0.0
   end
 
-  SolarMeasurements.new(solar_watts_current)
+  solar_watts_per_month = 0.0 # not implemented yet
+
+  SolarMeasurements.new(solar_watts_current, solar_watts_per_month)
 end
