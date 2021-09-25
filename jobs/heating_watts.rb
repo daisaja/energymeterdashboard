d74c6bf0bc31ccebd9295eb2b288a62c29090f13
Lars Gentsch <lars.gentsch@cloud.ionos.com>
2021-09-25 19:43:27 +0200

    Removed klimato because Yahoo stopped whether API support. Added send of data to influxdb and made it configurable.

--- jobs/heating_watts.rb
+++ jobs/heating_watts.rb
@@ -3,4 +3,15 @@ require_relative 'meter_helper/heating_meter_client'
 SCHEDULER.every '2s', :first_in => 0 do |job|
   heating_measurements = HeatingMeasurements.new()
   send_event('wattmeterheating',   { value: heating_measurements.heating_watts_current})
+  report_heating(heating_measurements.heating_watts_current)
+end
+
+def report_heating(heating_watts_current)
+  reporter = InfluxExporter.new()
+  hash = {
+           name: 'wattmeter_heating',
+           tags: {meter_type: 'heating'},
+           fields: {heating_watts_current: heating_watts_current},
+         }
+  reporter.send_data(hash)
 end
